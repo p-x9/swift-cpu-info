44d3076da43814fc4de4270c5ace0e521ab04683
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-21 23:43:16 +0900

    implemented `CPUThreadType`

---
+++ Sources/CPUInfo/Model/CPUThreadType.swift
@@ -0,0 +1,55 @@
+//
+//  CPUThreadType.swift
+//
+//
+//  Created by p-x9 on 2024/04/21.
+//  
+//
+
+import Foundation
+import SwiftSysctl
+
+public enum CPUThreadType {
+    /// CPU_THREADTYPE_NONE
+    case none
+    /// CPU_THREADTYPE_INTEL_HTT
+    case intel_htt
+}
+
+extension CPUThreadType: RawRepresentable {
+    public typealias RawValue = Int32
+
+    public init?(rawValue: RawValue) {
+        switch rawValue {
+        case CPU_THREADTYPE_NONE: self = .none
+        case CPU_THREADTYPE_INTEL_HTT: self = .intel_htt
+        default: return nil
+        }
+    }
+
+    public var rawValue: RawValue {
+        switch self {
+        case .none: CPU_THREADTYPE_NONE
+        case .intel_htt: CPU_THREADTYPE_INTEL_HTT
+        }
+    }
+}
+
+extension CPUThreadType: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .none: "CPU_THREADTYPE_NONE"
+        case .intel_htt: "CPU_THREADTYPE_INTEL_HTT"
+        }
+    }
+}
+
+extension CPUThreadType {
+    /// CPU thread type of host pc
+    static var current: CPUThreadType? {
+        guard let type = Sysctl.sysctl(hw.cputhreadtype) else {
+            return nil
+        }
+        return .init(rawValue: CPUThreadType.RawValue(type))
+    }
+}
