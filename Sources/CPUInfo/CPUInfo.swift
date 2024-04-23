ea580e1093db83a869729b952925c801d5974023
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-23 21:35:02 +0900

    Implemented common infos

--- Sources/CPUInfo/CPUInfo.swift
+++ Sources/CPUInfo/CPUInfo.swift
@@ -1,2 +1,50 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import SwiftSysctl
+import CPUInfoC
+
+public enum CPUInfo {}
+
+extension CPUInfo {
+    public static var type: CPUType? { .current }
+    public static var subType: CPUSubType? { .current }
+
+    public static var family: CPUFamily? { .current }
+    public static var subFamily: CPUSubFamily? { .current }
+
+    public static var threadType: CPUThreadType? { .current }
+}
+
+extension CPUInfo {
+    public static var is64Bit: Bool {
+        Sysctl.sysctl(hw.cpu64bit_capable) == 1
+    }
+}
+
+extension CPUInfo {
+    public static var brand: String? {
+        Sysctl.sysctl(machdep.cpu.brand_string)
+    }
+
+    public static var vendor: String? {
+        #if arch(x86_64)
+        Sysctl.sysctl(machdep.cpu.vendor)
+        #else
+        if let vendor = Sysctl.sysctl("machdep.cpu.vendor") {
+            return vendor.withUnsafeBytes {
+                guard let baseAddress = $0.baseAddress else {
+                    return nil
+                }
+                return String(
+                    cString: baseAddress
+                        .assumingMemoryBound(to: CChar.self)
+                )
+            }
+        }
+        if let brand,
+           brand.starts(with: "Apple ") {
+            return "Apple Inc."
+        }
+        return nil
+        #endif
+    }
+}
