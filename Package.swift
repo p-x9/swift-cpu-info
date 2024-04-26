a3fbb6270c4e45d594d4e4fc2107353fc0354f0f
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-26 14:01:58 +0900

    Bump `swift-sysctl` to 0.5.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/p-x9/swift-sysctl.git", from: "0.4.0")
+        .package(url: "https://github.com/p-x9/swift-sysctl.git", from: "0.5.0")
     ],
     targets: [
         .target(
