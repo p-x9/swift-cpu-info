e4ba58255f21620335f0823a1035780fbebd97e6
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-12-28 22:48:03 +0900

    Update ARM feature detection with additional sysctl fallbacks and links
    - Added fallbacks for several ARM features to improve compatibility
    - Updated legacy sysctl links for accuracy
    - Included new features from ID_AA64ISAR2_EL1 and ID_AA64MMFR1_EL1

--- Sources/CPUInfo/arm64/CPUInfo+features.swift
+++ Sources/CPUInfo/arm64/CPUInfo+features.swift
@@ -16,7 +16,8 @@ extension CPUInfo {
             ("arm64", try? Sysctl.sysctl(hw.optional.arm64)),
 
             /* Legacy Names ARM Optional Feature Sysctls */
-            // https://github.com/apple-oss-distributions/xnu/blob/94d3b452840153a99b38a3a9659680b2a006908e/bsd/kern/kern_mib.c#L1127
+            // https://github.com/apple-oss-distributions/xnu/blob/f6217f891ac0bb64f3d375211650a4c1ff8ca1ea/bsd/kern/kern_mib.c#L1150
+            // https://github.com/apple-oss-distributions/xnu/blob/f6217f891ac0bb64f3d375211650a4c1ff8ca1ea/bsd/kern/kern_mib.c#L1154
             ("neon", try? Sysctl.sysctl(hw.optional.neon)),
             ("neon_hpfp", try? Sysctl.sysctl(hw.optional.neon_hpfp)),
             ("neon_fp16", try? Sysctl.sysctl(hw.optional.neon_fp16)),
@@ -30,29 +31,31 @@ extension CPUInfo {
 //            ("watchpoint", try? Sysctl.sysctl(hw.optional.watchpoint)),
 //            ("breakpoint", try? Sysctl.sysctl(hw.optional.breakpoint)),
 
+            // https://github.com/apple-oss-distributions/xnu/blob/f6217f891ac0bb64f3d375211650a4c1ff8ca1ea/osfmk/arm/arm_features.inc#L36
             /* Features from: ID_AA64ISAR0_EL1 */
-            ("armv8_crc32", try? Sysctl.sysctl(hw.optional.armv8_crc32)),
+            ("CRC32", (try? Sysctl.sysctl(hw.optional.arm.FEAT_CRC32)) ?? (try? Sysctl.sysctl(hw.optional.armv8_crc32))),
             ("FlagM", try? Sysctl.sysctl(hw.optional.arm.FEAT_FlagM)),
             ("FlagM2", try? Sysctl.sysctl(hw.optional.arm.FEAT_FlagM2)),
-            ("FHM", try? Sysctl.sysctl(hw.optional.arm.FEAT_FHM)),
+            ("FHM", (try? Sysctl.sysctl(hw.optional.arm.FEAT_FHM)) ?? (try? Sysctl.sysctl(hw.optional.armv8_2_fhm))),
             ("DotProd", try? Sysctl.sysctl(hw.optional.arm.FEAT_DotProd)),
-            ("SHA3", try? Sysctl.sysctl(hw.optional.arm.FEAT_SHA3)),
+            ("SHA3", (try? Sysctl.sysctl(hw.optional.arm.FEAT_SHA3)) ?? (try? Sysctl.sysctl(hw.optional.armv8_2_sha3))),
             ("RDM", try? Sysctl.sysctl(hw.optional.arm.FEAT_RDM)),
-            ("LSE", try? Sysctl.sysctl(hw.optional.arm.FEAT_LSE)),
+            ("LSE", (try? Sysctl.sysctl(hw.optional.arm.FEAT_LSE)) ?? (try? Sysctl.sysctl(hw.optional.armv8_1_atomics))),
             ("SHA256", try? Sysctl.sysctl(hw.optional.arm.FEAT_SHA256)),
-            ("SHA512", try? Sysctl.sysctl(hw.optional.arm.FEAT_SHA512)),
+            ("SHA512", (try? Sysctl.sysctl(hw.optional.arm.FEAT_SHA512)) ?? (try? Sysctl.sysctl(hw.optional.armv8_2_sha512))),
             ("SHA1", try? Sysctl.sysctl(hw.optional.arm.FEAT_SHA1)),
             ("AES", try? Sysctl.sysctl(hw.optional.arm.FEAT_AES)),
             ("PMULL", try? Sysctl.sysctl(hw.optional.arm.FEAT_PMULL)),
 
             /* Features from: ID_AA64ISAR1_EL1 */
-            ("armv8_gpi", try? Sysctl.sysctl(hw.optional.armv8_gpi)),
             ("SPECRES", try? Sysctl.sysctl(hw.optional.arm.FEAT_SPECRES)),
+            ("SPECRES2", try? Sysctl.sysctl(hw.optional.arm.FEAT_SPECRES2)),
             ("SB", try? Sysctl.sysctl(hw.optional.arm.FEAT_SB)),
             ("FRINTTS", try? Sysctl.sysctl(hw.optional.arm.FEAT_FRINTTS)),
+            ("PACIMP", (try? Sysctl.sysctl(hw.optional.arm.FEAT_PACIMP)) ?? (try? Sysctl.sysctl(hw.optional.armv8_gpi))),
             ("LRCPC", try? Sysctl.sysctl(hw.optional.arm.FEAT_LRCPC)),
             ("LRCPC2", try? Sysctl.sysctl(hw.optional.arm.FEAT_LRCPC2)),
-            ("FCMA", try? Sysctl.sysctl(hw.optional.arm.FEAT_FCMA)),
+            ("FCMA", (try? Sysctl.sysctl(hw.optional.arm.FEAT_FCMA)) ?? (try? Sysctl.sysctl(hw.optional.armv8_3_compnum))),
             ("JSCVT", try? Sysctl.sysctl(hw.optional.arm.FEAT_JSCVT)),
             ("PAuth", try? Sysctl.sysctl(hw.optional.arm.FEAT_PAuth)),
             ("PAuth2", try? Sysctl.sysctl(hw.optional.arm.FEAT_PAuth2)),
@@ -60,21 +63,31 @@ extension CPUInfo {
             ("DPB", try? Sysctl.sysctl(hw.optional.arm.FEAT_DPB)),
             ("DPB2", try? Sysctl.sysctl(hw.optional.arm.FEAT_DPB2)),
             ("BF16", try? Sysctl.sysctl(hw.optional.arm.FEAT_BF16)),
+            ("EBF16", try? Sysctl.sysctl(hw.optional.arm.FEAT_EBF16)),
             ("I8MM", try? Sysctl.sysctl(hw.optional.arm.FEAT_I8MM)),
+
+            /* Features from: ID_AA64ISAR2_EL1 */
+            ("WFxT", try? Sysctl.sysctl(hw.optional.arm.FEAT_WFxT)),
+            ("RPRES", try? Sysctl.sysctl(hw.optional.arm.FEAT_RPRES)),
+            ("CSSC", try? Sysctl.sysctl(hw.optional.arm.FEAT_CSSC)),
+            ("HBC", try? Sysctl.sysctl(hw.optional.arm.FEAT_HBC)),
 
             /* Features from: ID_AA64MMFR0_EL1 */
             ("ECV", try? Sysctl.sysctl(hw.optional.arm.FEAT_ECV)),
+
+            /* Features from: ID_AA64MMFR1_EL1 */
+            ("AFP", try? Sysctl.sysctl(hw.optional.arm.FEAT_AFP)),
 
             /* Features from: ID_AA64MMFR2_EL1 */
             ("LSE2", try? Sysctl.sysctl(hw.optional.arm.FEAT_LSE2)),
 
             /* Features from: ID_AA64PFR0_EL1 */
-            ("AdvSIMD", try? Sysctl.sysctl(hw.optional.AdvSIMD)),
-            ("AdvSIMD_HPFPCvt", try? Sysctl.sysctl(hw.optional.AdvSIMD_HPFPCvt)),
             ("CSV2", try? Sysctl.sysctl(hw.optional.arm.FEAT_CSV2)),
             ("CSV3", try? Sysctl.sysctl(hw.optional.arm.FEAT_CSV3)),
             ("DIT", try? Sysctl.sysctl(hw.optional.arm.FEAT_DIT)),
-            ("FP16", try? Sysctl.sysctl(hw.optional.arm.FEAT_FP16)),
+            ("AdvSIMD", (try? Sysctl.sysctl(hw.optional.arm.AdvSIMD)) ?? (try? Sysctl.sysctl(hw.optional.AdvSIMD)) ?? (try? Sysctl.sysctl(hw.optional.neon))),
+            ("AdvSIMD_HPFPCvt", (try? Sysctl.sysctl(hw.optional.arm.AdvSIMD_HPFPCvt)) ?? (try? Sysctl.sysctl(hw.optional.AdvSIMD_HPFPCvt)) ?? (try? Sysctl.sysctl(hw.optional.neon_hpfp))),
+            ("FP16", (try? Sysctl.sysctl(hw.optional.arm.FEAT_FP16)) ?? (try? Sysctl.sysctl(hw.optional.neon_fp16))),
 
             /* Features from: ID_AA64PFR1_EL1 */
             ("SSBS", try? Sysctl.sysctl(hw.optional.arm.FEAT_SSBS)),
